232e1fb6805e4fd933a5b79447d8ab9771b3a400
Starfium <Starfium@163.com>
2026-02-17 19:03:44 +0800

    添加右侧标题和描述的打字机效果

--- static/main.js
+++ static/main.js
@@ -1,1 +1,24 @@
-console.log("Hello, Starfium!");
+function typeWriter(element, text, delay) {
+    const textElement=document.getElementById(element);
+    let i = textElement.textContent.length; // 从当前文本长度开始
+    let len = text.length;
+    if (i<len) {
+        textElement.textContent += text.charAt(i);
+        i++;
+        setTimeout(() => typeWriter(element, text, delay), delay);
+    }
+}
+function randomText(){
+    const texts = [
+        "小保底不要歪 小保底不要歪 小保底不要歪",
+        "lzyz 真是 sm",
+        "我是逆蝶",
+        "被砖头砸晕算不算急性硅酸盐中毒",
+        "浩洋先生沉鱼落雁闭月羞花",
+    ]
+    return texts[Math.floor(Math.random() * texts.length)];
+}
+document.addEventListener('DOMContentLoaded', function() {
+    typeWriter("right-title", "Hello, I'm Starfium", 60);
+    typeWriter("right-description", randomText(), 40);
+});
